799cc3dadc0d4f2d64ce213cfbfc29f935e5fe08
Sooraj Gupta <soorajgupta00@gmail.com>
2024-01-09 03:51:09 +0530

    finished carousel

--- js/script.js
+++ js/script.js
@@ -249,6 +249,50 @@ function displayBackgroundImage(type, background_path) {
   }
 }
 
+// Display Slider
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+    `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      // disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Fetch Data from MovieDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = "034762d6f969bde75ad8302b2c2e63a5";
@@ -292,6 +336,7 @@ function init() {
     case "/":
     case "/index.html":
       // console.log("Home");
+      displaySlider();
       DisplayPopularMovies();
       break;
     case "/shows.html":
