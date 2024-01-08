bf8d634bb9879bbca66cd81bec19f1bbb0a4f84a
Sooraj Gupta <soorajgupta00@gmail.com>
2024-01-08 21:11:20 +0530

    finished tv-detail & added backdrops

--- js/script.js
+++ js/script.js
@@ -86,8 +86,11 @@ async function displayMovieDetails() {
   // console.log(movieID);
   const movie = await fetchAPIData(`movie/${movieID}`);
 
+  // Overlay for BackGround Image
+  displayBackgroundImage("movie", movie.backdrop_path);
+
   const div = document.createElement("div");
-  console.log(movie);
+  // console.log(movie);
   div.innerHTML = `
   <div class="details-top">
           <div>
@@ -111,7 +114,7 @@ async function displayMovieDetails() {
             <h2>${movie.title}</h2>
             <p>
               <i class="fas fa-star text-primary"></i>
-              8 / 10
+              ${movie.vote_average.toFixed(1)} / 10
             </p>
             <p class="text-muted">Release Date: ${movie.release_date}</p>
             <p>
@@ -150,6 +153,100 @@ async function displayMovieDetails() {
   `;
 
   document.querySelector("#movie-details").appendChild(div);
+}
+
+// Display Show Details
+async function displayShowDetails() {
+  const showID = window.location.search.split("=")[1];
+  // console.log(movieID);
+  const show = await fetchAPIData(`tv/${showID}`);
+
+  // Overlay for BackGround Image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+  // console.log(movie);
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            show.poster_path
+              ? `<img
+                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                class="card-img-top"
+                alt="${show.name}"
+              />`
+              : `
+              <img
+                src="images/no-image.jpg"
+                class="card-img-top"
+                alt="${show.name}"
+              />
+              `
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li><span class="text-secondary">Last Episode to Air:</span> ${
+              show.last_episode_to_air.name
+            }</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+            ${show.production_companies
+              .map((company) => `<div>${company.name}</div>`)
+              .join("")}
+          </div>
+        </div>
+  `;
+
+  document.querySelector("#show-details").appendChild(div);
+}
+
+// Display BackDrop on Details Page
+function displayBackgroundImage(type, background_path) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://images.tmdb.org/t/p/original/${background_path})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.125";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
 
 // Fetch Data from MovieDB API
@@ -206,7 +303,8 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      // console.log("TV Details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
